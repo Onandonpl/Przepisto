f9a8cdd672ca70e3141038e5786990c6df94a97c
Robert Mehring <onandonpl@gmail.com>
2021-03-22 22:41:40 +0100

    add providers

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { AuthProvider } from "./context/AuthContext";
+import { TagsProvider } from "./context/TagsContext";
+import { IngredientsProvider } from "./context/IngredientsContext";
 ReactDOM.render(
   <AuthProvider>
-    <App />
+    <TagsProvider>
+      <IngredientsProvider>
+        <App />
+      </IngredientsProvider>
+    </TagsProvider>
   </AuthProvider>,
   document.getElementById("root")
 );
