361ace13372172eb4c263e3d48b7f547433cab11
Robert Mehring <onandonpl@gmail.com>
2021-04-01 15:51:00 +0200

    update routes

--- src/context/Providers.js
+++ src/context/Providers.js
@@ -2,13 +2,16 @@ import React from "react";
 import { AuthProvider } from "./AuthContext";
 import { TagsProvider } from "./TagsContext";
 import { IngredientsProvider } from "./IngredientsContext";
+import { RecipeAddProvider } from "./RecipeAddContext";
 
 const Providers = ({ children }) => {
   return (
     <AuthProvider>
-      <TagsProvider>
-        <IngredientsProvider>{children}</IngredientsProvider>
-      </TagsProvider>
+      <RecipeAddProvider>
+        <TagsProvider>
+          <IngredientsProvider>{children}</IngredientsProvider>
+        </TagsProvider>
+      </RecipeAddProvider>
     </AuthProvider>
   );
 };
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,21 +2,49 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import UserInformation from "../views/User/UserInformation";
 import PrivateRoute from "./PrivateRoute";
-import Error from "../views/ErrorPage/Error";
+import ErrorPage from "../views/ErrorPage";
 import Home from "../views/Home";
 import Login from "../views/Auth/Login";
 import Register from "../views/Auth/Register";
-import StepsRoutes from "./StepsRoutes";
+import {
+  Images,
+  Ingredients,
+  Instruction,
+  Name,
+  Tags,
+  Finish,
+} from "../views/RecipeAdd";
+
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home}></Route>
       <Route exact path="/login" component={Login}></Route>
       <Route exact path="/register" component={Register}></Route>
-
       <PrivateRoute exact path="/user" component={UserInformation} />
-      <StepsRoutes />
-      <Route component={Error} />
+      <PrivateRoute exact path="/recipe-name" component={Name}></PrivateRoute>
+      <PrivateRoute
+        exact
+        path="/recipe-finish"
+        component={Finish}
+      ></PrivateRoute>
+      <PrivateRoute exact path="/recipe-tags" component={Tags}></PrivateRoute>
+      <PrivateRoute
+        exact
+        path="/recipe-ingredients"
+        component={Ingredients}
+      ></PrivateRoute>
+      <PrivateRoute
+        exact
+        path="/recipe-steps"
+        component={Instruction}
+      ></PrivateRoute>
+      <PrivateRoute
+        exact
+        path="/recipe-images"
+        component={Images}
+      ></PrivateRoute>{" "}
+      <Route component={ErrorPage} />
     </Switch>
   );
 };
