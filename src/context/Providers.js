2141a9159cb2baadba26cfc87f182dc59ac0210a
Robert Mehring <onandonpl@gmail.com>
2021-04-06 11:04:07 +0200

    add recipes provider

--- src/context/Providers.js
+++ src/context/Providers.js
@@ -3,15 +3,18 @@ import { AuthProvider } from "./AuthContext";
 import { TagsProvider } from "./TagsContext";
 import { IngredientsProvider } from "./IngredientsContext";
 import { RecipeAddProvider } from "./RecipeAddContext";
+import { RecipesProvider } from "./RecipesContext";
 
 const Providers = ({ children }) => {
   return (
     <AuthProvider>
-      <RecipeAddProvider>
-        <TagsProvider>
-          <IngredientsProvider>{children}</IngredientsProvider>
-        </TagsProvider>
-      </RecipeAddProvider>
+      <RecipesProvider>
+        <RecipeAddProvider>
+          <TagsProvider>
+            <IngredientsProvider>{children}</IngredientsProvider>
+          </TagsProvider>
+        </RecipeAddProvider>
+      </RecipesProvider>
     </AuthProvider>
   );
 };
