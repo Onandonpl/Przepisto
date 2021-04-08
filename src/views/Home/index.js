ad9b8e1f592a7673192045d4c9456f072b6a3f52
Robert Mehring <onandonpl@gmail.com>
2021-04-08 18:56:17 +0200

    delete console log, rename component

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -2,18 +2,17 @@ import React from "react";
 import { useRecipes } from "../../context/RecipesContext";
 
 import PageTitle from "../../components/pageTitle";
-import RecipeSmall from "../../components/RecipeSmall";
+import RecipeThumb from "./components/RecipeThumb/RecipeThumb";
 import { Recipes } from "./style";
 const Home = () => {
   const recipes = useRecipes();
-  console.log(recipes.recipes);
   return (
     <PageTitle helmet={"Strona główna"}>
       <Recipes>
         {recipes.recipes.map((recipe) => {
           const { recipeID, recipeData } = recipe;
           return (
-            <RecipeSmall key={recipeID} recipeID={recipeID} data={recipeData} />
+            <RecipeThumb key={recipeID} recipeID={recipeID} data={recipeData} />
           );
         })}
       </Recipes>
