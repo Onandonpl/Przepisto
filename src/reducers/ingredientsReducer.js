304bc2b28132cdad3e841a81aed5f0722fa17e33
Robert Mehring <onandonpl@gmail.com>
2021-04-08 18:55:50 +0200

    add dispatch

--- src/reducers/ingredientsReducer.js
+++ src/reducers/ingredientsReducer.js
@@ -1,9 +1,20 @@
+import { db } from "../lib/firebase";
 const emptyIngredients = { ingredients: [] };
+
+const handleAddIngredient = (data) => {
+  db.collection("ingredients").doc().set(data);
+};
 
 export const ingredientsReducer = (state, action) => {
   switch (action.type) {
     case "SET_INGREDIENTS":
       return { ...state, ingredients: action.payload };
+    case "ADD_INGREDIENT":
+      handleAddIngredient({
+        name: action.payload.name,
+        unit: action.payload.unit,
+      });
+      return { ...state };
 
     default:
       throw new Error(`Unknown action ${action.type}`);
