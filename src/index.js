f96e3f475552f23ea83a628b4f5402eec4400544
Robert Mehring <onandonpl@gmail.com>
2021-04-08 18:55:06 +0200

    add eslint disable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+// eslint-disable-next-line no-unused-vars
 import reportWebVitals from "./reportWebVitals";
 
 import Providers from "./context/Providers";
